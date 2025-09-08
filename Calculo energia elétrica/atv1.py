6a594589db209148b1a88efb420bf685562c5559
felipe-py <luis.silvareal77@gmail.com>
2025-09-07 21:08:13 -0300

    feat: Reformulando cálculos de densidade

--- Calculo energia elétrica/atv1.py
+++ Calculo energia elétrica/atv1.py
@@ -10,7 +10,7 @@ import os
 #TODO OK : 4- calcular a área total acesa de acordo com a quantidade de pixels acesos (área por pixel * numero de pixels acesos) 
 #TODO OK : 5 - calcular a densidade de pixels acesos de cada região
 #TODO OK : 6- calcular densidade média total
-#TODO OK  : 7- dividir a densidade de cada região com a densidade total 
+#TODO OK : 7- dividir a densidade de cada região com a densidade total 
 #TODO    : 8- se quiser, comparar com a porcentagem da área acesa (que não significa densidade)
 
 dic_area_cada_regiao = {
@@ -87,34 +87,34 @@ def calcula_area_pixel_acesos(lit_pixels, area_pixel_em_km_quadrado):
     area_pixel_aceso = area_pixel_em_km_quadrado * lit_pixels
     return area_pixel_aceso
 
-def calcula_densidade_geografica_regiao(nome_arquivo, lit_pixels):      # Duvida se usa aqui total de pixels ou pixels acesos
+def calcula_densidade_geografica_regiao(area_pixel_aceso_regiao, nome_arquivo):      # Duvida se usa aqui total de pixels ou pixels acesos
     """Calcula a densidade geográfica de pixels por km².
     
     * Utiliza variável global dic_area_cada_regiao.
 
     Args:
         nome_arquivo (str): Nome do arquivo da imagem
-        lit_pixels (number): Total de pixels acesos da imagem
+        area_pixel_em_km_quadrado (number): Área total de pixels acesos da imagem em km²
 
     Returns:
         number: Densidade geográfica de pixels por km²
     """
     area_regiao = dic_area_cada_regiao[nome_arquivo]
-    densidade_geografica = lit_pixels / area_regiao 
+    densidade_geografica = area_pixel_aceso_regiao / area_regiao
     return densidade_geografica
 
 
-def calcula_densidade_geografica_mundial(total_lit_pixels, total_area_mundial):
+def calcula_densidade_geografica_mundial(area_total_iluminada, total_area_mundial):
     """Calcula a densidade geográfica mundial de pixels por km².
 
     Args:
-        total_lit_pixels (number): Total de pixels acesos em todas as imagens
+        area_total_iluminada (number): Área total de pixels acesos em todas as imagens em km²
         total_area_mundial (number): Área total do mundo em km²
 
     Returns:
-        number: Densidade geográfica mundial de pixels por km²
+        number: Densidade geográfica mundial de pixels em porcentagem
     """
-    densidade_geografica_mundial = total_lit_pixels / total_area_mundial
+    densidade_geografica_mundial = (area_total_iluminada / total_area_mundial)
     return densidade_geografica_mundial
 
 
@@ -138,6 +138,7 @@ def gerar_relatorio(pasta):
     arquivos = os.listdir(pasta)
     relatorio_geral = {}
     total_pixels_acesos_mundo = 0
+    total_area_iluminada = 0
 
     # Primeiro loop: processa cada imagem
     for nome_arquivo in arquivos:
@@ -148,7 +149,10 @@ def gerar_relatorio(pasta):
         total_pixels_acesos_mundo += lit_pixels
         
         area_pixel_em_km_quadrado = calcula_area_pixel(nome_arquivo, total_pixels)
-        densidade_demografica_regiao = calcula_densidade_geografica_regiao(nome_arquivo, lit_pixels)
+        area_pixel_aceso_regiao = calcula_area_pixel_acesos(lit_pixels, area_pixel_em_km_quadrado)
+        total_area_iluminada += area_pixel_aceso_regiao
+        
+        densidade_demografica_regiao = calcula_densidade_geografica_regiao(area_pixel_aceso_regiao, nome_arquivo)
 
         relatorio_geral[nome_arquivo] = {
             "total_pixels": total_pixels,
@@ -156,18 +160,18 @@ def gerar_relatorio(pasta):
             "percentual": percentual,
             "altura": altura,
             "largura": largura,
-            "area_pixels_acesos": calcula_area_pixel_acesos(lit_pixels, area_pixel_em_km_quadrado),
+            "area_pixels_acesos": area_pixel_aceso_regiao,
             "area_pixel_em_km_quadrado": area_pixel_em_km_quadrado,
             "densidade_geografica_regiao": densidade_demografica_regiao,
             "indice_densidade_relativa": None  # Será calculado depois
         }
+        
+    densidade_geografica_mundial = calcula_densidade_geografica_mundial(total_area_iluminada, total_area_mundial)
     
     relatorio_geral["info_mundial"] = {
         "total_pixels_acesos_mundo": total_pixels_acesos_mundo,
-        "total_densidade_mundo": calcula_densidade_geografica_mundial(total_pixels_acesos_mundo, total_area_mundial)
+        "total_densidade_mundo": densidade_geografica_mundial * 100  # Convertendo para porcentagem aqui, não prejudica no cálculo da função
     }
-    
-    densidade_geografica_mundial = calcula_densidade_geografica_mundial(total_pixels_acesos_mundo, total_area_mundial)
 
     for nome_arquivo in arquivos:
         relatorio_geral[nome_arquivo]["indice_densidade_relativa"] = calcula_indice_densidade_relativa(relatorio_geral[nome_arquivo]["densidade_geografica_regiao"], densidade_geografica_mundial)
@@ -182,19 +186,19 @@ def exibir_relatorio(relatorio_geral):
     print("="*50)
     print("Informações mundiais calculadas:\n")
     print(f"Total de pixels acesos no mundo: {info_mundial['total_pixels_acesos_mundo']}\n")
-    print(f"Densidade geográfica mundial: {info_mundial['total_densidade_mundo']:.6f} pixels/km²\n")
+    print(f"Densidade geográfica mundial: {info_mundial['total_densidade_mundo']:.2f} %\n")
     
     for nome, valores in relatorio_geral.items():
         print("="*50)
         print(
             f"\n{nome.upper()}\n\n"
+            f"Altura: {valores['altura']}\n"
+            f"Largura: {valores['largura']}\n\n"
             f"total de pixels: {valores['total_pixels']}\n"
             f"total pixels acesos: {valores['lit_pixels']}\n"
-            f"percentual de pixels acesos: {valores['percentual']:.2f}%\n"
-            f"altura da imagem: {valores['altura']} pixels\n"
-            f"largura da imagem: {valores['largura']} pixels\n"
-            f"área total de pixels acesos: {valores['area_pixels_acesos']:.2f} km²\n"
+            f"percentual de pixels acesos: {valores['percentual']:.2f}%\n\n"
             f"área de cada pixel: {valores['area_pixel_em_km_quadrado']:.6f} km²\n"
+            f"área total de pixels acesos: {valores['area_pixels_acesos']:.2f} km²\n\n"
             f"densidade geográfica: {valores['densidade_geografica_regiao']:.6f} pixels/km²\n"
             f"índice de densidade relativa: {valores['indice_densidade_relativa']:.6f}\n"
         )
